6db6ed72ae822a0a3cede0441c478f7cdd083c70
Giorgi <giorgi.kharshila@gmail.com>
2018-02-08 22:27:47 -0500

    Part 1 Done

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 def intial_state(rows, cols, row_pieces):
 
 	state = []
@@ -6,10 +8,10 @@ def intial_state(rows, cols, row_pieces):
 
 		raise Exception("Invalid number of Row Pieces")
 
-	for i in range(0,rows):
+	for i in range(rows):
 		state.append([])
 
-		for j in range(0, cols):
+		for j in range(cols):
 			state[i].append(".")
 
 	row_piece = row_pieces // 2
@@ -29,6 +31,72 @@ def display_state(state):
 	for row in state:
 		print(''.join(row))
 
+def transition(state, start, finish):
+
+	i_end = finish[0]
+	j_end = finish[1]
+
+	for i in range(len(state)):
+		for j in range(len(state[i])):
+			if (i, j) == start:
+				state[i_end][j_end] = state[i][j]
+				state[i][j] = "."
+
+	return state
 
 
-display_state(intial_state(8, 8, 4))
+def directions(state, player, position):
+
+	i = position[0]
+	j = position[1]
+
+	my_moves = []
+
+	if player == "X" and i < len(state)-1:
+
+		# Right
+		if j > 0 and state[i+1][j-1] != "X":
+			my_moves.append((i+1, j-1))
+		# Forward
+		if state[i+1][j] != "X":
+			my_moves.append((i+1, j))
+		# Left
+		if j < len(state[i])-1 and state[i+1][j+1] != "X":
+			my_moves.append((i+1, j+1))
+
+	elif player == "O" and i != 0:
+
+		# Left
+		if j > 0 and state[i-1][j-1] != "O":
+			my_moves.append((i-1, j-1))
+		# Forward
+		if state[i-1][j] != "O":
+			my_moves.append((i-1, j))
+		# Right
+		if j < len(state[i])-1 and state[i-1][j+1] != "O":
+			my_moves.append((i-1, j+1))
+
+	return my_moves
+
+
+def move_generator(state, player):
+
+	all_moves = {}
+
+	for i in range(len(state)):
+		for j in range(len(state[i])):
+			all_moves[(i, j)] = directions(state, player, (i, j))
+
+	return all_moves
+
+
+
+if __name__ == '__main__':
+
+	my_state = intial_state(8, 8, 4)
+	
+	# tran_state = transition(my_state, (6,0), (0,0))
+
+	# print(move_generator(my_state, "O"))
+
+	display_state(my_state)
